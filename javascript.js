339faed7ddf7d993451894584f016df380ecaf29
Arhchu <71408816+abaskarasingham@users.noreply.github.com>
2025-08-03 21:31:29 -0400

    Add getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    const choice = Math.floor(Math.random() * 3);
+    let choiceStr = "";
+
+    switch(choice) {
+        case 0: choiceStr = "rock";
+        break;
+        case 1: choiceStr = "paper";
+        break;
+        case 2: choiceStr = "scissors";
+        break;
+    }
+
+    return choiceStr;
+}
+
+console.log(getComputerChoice());
