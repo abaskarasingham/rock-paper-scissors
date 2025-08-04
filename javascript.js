14ec84f4d7f75c023d6036dc28afaedbfbfd065e
Arhchu <71408816+abaskarasingham@users.noreply.github.com>
2025-08-03 23:30:08 -0400

    Add playGame function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     const choice = Math.floor(Math.random() * 3);
     let choiceStr = "";
@@ -21,36 +18,52 @@ function getHumanChoice() {
     return prompt("Select rock, paper, or scissors");
 }
 
-function playRound(humanChoice, computerChoice) {
-    const human = humanChoice.toLowerCase();
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
     let result = "";
 
-    if(human === "rock" && computerChoice === "scissors") {
-        result = "You win! Rock beats Scissors";
-        ++humanScore;
-    } else if (human === "rock" && computerChoice === "paper") {
-        result = "You lose! Paper beats Rock";
-        ++computerScore;
-    } else if (human === "paper" && computerChoice === "rock") {
-        result = "You win! Paper beats Rock";
-        ++humanScore;
-    } else if (human === "paper" && computerChoice === "scissors") {
-        result = "You lose! Scissors beats Paper";
-        ++computerScore;
-    } else if (human === "scissors" && computerChoice === "paper") {
-        result = "You win! Scissors beats Paper";
-        ++humanScore;
-    } else if (human === "scissors" && computerChoice === "rock") {
-        result = "You lose! Rock beats Scissors";
-        ++computerScore;
-    } else {
-        result = `You tied! ${human.substring(0,1).toUpperCase() + human.substring(1)} ties ${computerChoice.substring(0,1).toUpperCase() + computerChoice.substring(1)}`;
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+        let result = "";
+
+        if (humanChoice === "rock" && computerChoice === "scissors") {
+            result = "You win! Rock beats Scissors";
+            ++humanScore;
+        } else if (humanChoice === "rock" && computerChoice === "paper") {
+            result = "You lose! Paper beats Rock";
+            ++computerScore;
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
+            result = "You win! Paper beats Rock";
+            ++humanScore;
+        } else if (humanChoice === "paper" && computerChoice === "scissors") {
+            result = "You lose! Scissors beats Paper";
+            ++computerScore;
+        } else if (humanChoice === "scissors" && computerChoice === "paper") {
+            result = "You win! Scissors beats Paper";
+            ++humanScore;
+        } else if (humanChoice === "scissors" && computerChoice === "rock") {
+            result = "You lose! Rock beats Scissors";
+            ++computerScore;
+        } else {
+            result = `You tied! ${humanChoice.substring(0, 1).toUpperCase() + humanChoice.substring(1)} ties ${computerChoice.substring(0, 1).toUpperCase() + computerChoice.substring(1)}`;
+        }
+
+        console.log(result);
     }
+
+    for (let i=0; i<5; ++i) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    if (humanScore > computerScore) {
+        result = `You won the game!\n`;
+    } else {
+        result = `You lost the game!\n`;
+    }
+    result += `Your Score: ${humanScore}\nComputer Score: ${computerScore}`;
 
     console.log(result);
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
