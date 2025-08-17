f5cf21144d66d023d71ea6b9f2bd38d4d2172116
Arhchu <71408816+abaskarasingham@users.noreply.github.com>
2025-08-17 12:12:43 -0400

    Add UI and CSS styling
    The player will make their selection through
    buttons instead of typing. CSS Styling was added
    to make the website look better. The game now ends
    once a player reaches 5 points.

--- javascript.js
+++ javascript.js
@@ -18,52 +18,64 @@ function getHumanChoice() {
     return prompt("Select rock, paper, or scissors");
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
     let result = "";
 
-    function playRound(humanChoice, computerChoice) {
-        humanChoice = humanChoice.toLowerCase();
-        let result = "";
-
-        if (humanChoice === "rock" && computerChoice === "scissors") {
-            result = "You win! Rock beats Scissors";
-            ++humanScore;
-        } else if (humanChoice === "rock" && computerChoice === "paper") {
-            result = "You lose! Paper beats Rock";
-            ++computerScore;
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
-            result = "You win! Paper beats Rock";
-            ++humanScore;
-        } else if (humanChoice === "paper" && computerChoice === "scissors") {
-            result = "You lose! Scissors beats Paper";
-            ++computerScore;
-        } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            result = "You win! Scissors beats Paper";
-            ++humanScore;
-        } else if (humanChoice === "scissors" && computerChoice === "rock") {
-            result = "You lose! Rock beats Scissors";
-            ++computerScore;
-        } else {
-            result = `You tied! ${humanChoice.substring(0, 1).toUpperCase() + humanChoice.substring(1)} ties ${computerChoice.substring(0, 1).toUpperCase() + computerChoice.substring(1)}`;
-        }
-
-        console.log(result);
-    }
-
-    for (let i=0; i<5; ++i) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-
-    if (humanScore > computerScore) {
-        result = `You won the game!\n`;
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+        result = "You win! Rock beats Scissors";
+        ++humanScore;
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        result = "You lose! Paper beats Rock";
+        ++computerScore;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        result = "You win! Paper beats Rock";
+        ++humanScore;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        result = "You lose! Scissors beats Paper";
+        ++computerScore;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        result = "You win! Scissors beats Paper";
+        ++humanScore;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        result = "You lose! Rock beats Scissors";
+        ++computerScore;
     } else {
-        result = `You lost the game!\n`;
+        result = `You tied! ${humanChoice.substring(0, 1).toUpperCase() + humanChoice.substring(1)} ties ${computerChoice.substring(0, 1).toUpperCase() + computerChoice.substring(1)}`;
     }
-    result += `Your Score: ${humanScore}\nComputer Score: ${computerScore}`;
+    result += `\nYour Score: ${humanScore}\nComputer Score: ${computerScore}\n`;
+    const htmlContent = result.replace(/\n/g, "<br>");
 
     console.log(result);
+    divResult.innerHTML = htmlContent;
 }
 
-playGame();
+function playGame() {
+    if (humanScore > computerScore) {
+        result = `You won the game!`;
+    } else {
+        result = `You lost the game!`;
+    }
+    
+    console.log(result);
+    divResult.innerHTML += result;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+let result = "";
+const MAX_POINTS = 5;
+
+const divResult = document.querySelector(".result");
+const allButtons = Array.from(document.querySelectorAll(".buttons > button"));
+const buttons = document.querySelector(".buttons");
+
+buttons.addEventListener("click", function (e) {
+    playRound(e.target.id, getComputerChoice());
+    if (humanScore === MAX_POINTS || computerScore === MAX_POINTS) {
+        playGame();
+        allButtons.forEach(button => {
+            button.disabled = true;
+        });
+    }
+});
